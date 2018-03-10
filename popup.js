63ffac09bb571453e8fa56482c8da928c0e5e955
tig <example@example.com>
2018-03-10 18:05:39 +0900

    広告仮組み込み

--- popup.js
+++ popup.js
@@ -53,6 +53,8 @@ document.addEventListener('DOMContentLoaded', () => {
 		
 		$createBtn.click(onClickCreatingGradeList);
 		$moveBtn.click(() => {window.open('https://p.eagate.573.jp/game/nostalgia/nst/playdata/entrance.html?k=music_data');});
+		
+		
 	});
 });
 
@@ -72,4 +74,123 @@ function onClickCreatingGradeList() {
 
 function isMusicDataPage(url) {
 	return url && url.match(/(p\.eagate\.573\.jp).*(entrance\.html\?k=music_data)/);
+}
+
+
+function showAd() {
+	
+	var url = makeAdURL();
+	
+	retryable(5, fetchAd.bind(url)).catch(function() {
+		console.log('広告取得失敗');
+	});
+}
+
+// Promiseを使ってリトライする
+function retryable(retryCount, func) {
+	let promise = Promise.reject().catch(() => func());
+	for (let i = 0; i < retryCount; i++) {
+		promise = promise.catch(err => func());
+	}
+	return promise;
+}
+
+function fetchAd() {
+	var url = this;
+	return new Promise(function(resolve, reject) {
+		$.ajax({
+			url:url,
+			type:'GET'
+		})
+		.done(function(data){
+			var $xml = $(data);
+			var $items = $xml.find("Item");
+			
+			var randIdx = getRandomInt(0, 1); // 上位２つまでが対象
+			var $item = $items.eq(randIdx);
+			var url = $item.find("DetailPageURL").text();
+			var imgUrl = $item.find("MediumImage URL").text();
+			var title = $item.find("ItemAttributes Title").text();
+			
+			$img = $("<img>").attr("src", imgUrl);
+			$title = $("<p>").text(title);
+			$link = $("<a>").attr("href", url).attr("target", "_blank");
+			$link.append($img).append($title);
+			$("#ad-area").append($link);
+			
+			resolve();
+		})
+		.fail(function(){
+			reject();
+		});
+	});
+}
+
+function makeAdURL() {
+	// Your Access Key ID, as taken from the Your Account page
+	var access_key_id = "";
+
+	// Your Secret Key corresponding to the above ID, as taken from the Your Account page
+	var secret_key = "";
+
+	// The region you are interested in
+	var endpoint = "webservices.amazon.co.jp";
+
+	var uri = "/onca/xml";
+
+	var params = {
+		"Service" : "AWSECommerceService",
+		"Operation" : "ItemSearch",
+		"AWSAccessKeyId" : "AKIAIF3HXGCCUNGYCCMA",
+		"AssociateTag" : "testparam083-22",
+		"SearchIndex" : "Music",
+		"Keywords" : getRandomKeyword(),
+		"Sort" : "-orig-rel-date",
+		"ResponseGroup" : "Images,ItemAttributes,Offers"
+	};
+
+	// Set current timestamp if not set
+	if (params["Timestamp"] == undefined) {
+		params["Timestamp"] = new Date().toISOString().slice(0, -5);
+	}
+	
+	var pairs = [];
+	
+	$.each(Object.keys(params).sort(), function() {
+		var key = this;
+		var value = params[key];
+		pairs.push(encodeURIComponent(key) + "=" + encodeURIComponent(value));
+	});
+
+	// Generate the canonical query
+	var canonical_query_string = pairs.join('&');
+
+	// Generate the string to be signed
+	var string_to_sign = "GET\n" + endpoint + "\n" + uri + "\n" + canonical_query_string;
+
+	// Generate the signature required by the Product Advertising API
+	var signature = CryptoJS.HmacSHA256(string_to_sign, secret_key).toString(CryptoJS.enc.Base64);
+
+	// Generate the signed URL
+	return 'http://'+endpoint+uri+'?'+canonical_query_string+'&Signature='+encodeURIComponent(signature);
+}
+
+function getRandomInt(min, max) {
+  return Math.floor( Math.random() * (max - min + 1) ) + min;
+}
+
+function getRandomKeyword() {
+	
+	var keywords = [
+		"KONAMI",
+		"BEMANI",
+		"GITADORA",
+		"jubeat",
+		"maimai",
+		"CHUNITHM"
+	];
+	
+	var max = keywords.length - 1;
+	
+	return keywords[getRandomInt(0, max)];
 }
