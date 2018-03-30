5c117991b652742caddd82da1459d4ca5cb6b434
tig <example@example.com>
2018-03-31 08:55:09 +0900

    楽曲データリストから取得する情報として音符別成功率と累計データを追加

--- content_script.js
+++ content_script.js
@@ -1,6 +1,135 @@
+const NORMAL = 0;
+const HARD = 1;
+const EXPERT = 2;
+const DIFFICULTY_LIST = [
+	NORMAL,
+	HARD,
+	EXPERT
+];
 
-function getScore(seq, key) {
-	return $("[data-pdataMap~='music_list.music[" + seq + "].sheet[2]." + key + "']").attr("data-pdataval");
+function getScore(seq, difficulty, key) {
+	return $("[data-pdataMap~='music_list.music[" + seq + "].sheet[" + difficulty + "]." + key + "']").attr("data-pdataval");
+}
+
+const SCORE_CONFIG = {
+	best_judge_good: {
+		name: "good",
+		filter: function(value) {
+			return parseInt(value);
+		}
+	},
+	best_judge_just: {
+		name: "just",
+		filter: function(value) {
+			return parseInt(value);
+		}
+	},
+	best_judge_miss: {
+		name: "miss",
+		filter: function(value) {
+			return parseInt(value);
+		}
+	},
+	best_judge_near: {
+		name: "near",
+		filter: function(value) {
+			return parseInt(value);
+		}
+	},
+	best_judge_superjust: {
+		name: "sjust",
+		filter: function(value) {
+			return parseInt(value);
+		}
+	},
+	best_score: {
+		name: "score",
+		filter: function(value) {
+			return parseInt(value);
+		}
+	},
+	best_time: {
+		name: "datetime",
+		filter: function(value) {
+			return value + ":00";
+		}
+	},
+	clear_count: {
+		name: "clearCount",
+		filter: function(value) {
+			return parseInt(value);
+		}
+	},
+	fc_type: {
+		name: "fc",
+	},
+	fullcombo_count: {
+		name: "fcCount",
+		filter: function(value) {
+			return parseInt(value);
+		}
+	},
+	grade: {
+		name: "grade",
+		filter: function(value) {
+			return insertStr(value, value.length - 2, '.'); // Floatにすると誤差がでるので文字列のまま
+		}
+	},
+	level: {
+		name: "level",
+		filter: function(value) {
+			return parseInt(value);
+		}
+	},
+	max_combo: {
+		name: "combo",
+		filter: function(value) {
+			return parseInt(value);
+		}
+	},
+	notes_glissando_rate: {
+		name: "glissandoRate",
+	},
+	notes_standard_rate: {
+		name: "standardRate",
+	},
+	notes_tenuto_rate: {
+		name: "tenutoRate",
+	},
+	notes_trill_rate: {
+		name: "trillRate",
+	},
+	pianistic_count: {
+		name: "pianisticCount",
+		filter: function(value) {
+			return parseInt(value);
+		}
+	},
+	play_count: {
+		name: "playCount",
+		filter: function(value) {
+			return parseInt(value);
+		}
+	},
+	rank: {
+		name: "rank",
+	},
+}
+
+function getScores(seq, difficulty) {
+	var scores = {};
+	$("[data-pdataMap*='music_list.music[" + seq + "].sheet[" + difficulty + "].']").each(function() {
+		var $el = $(this);
+		var pdataMap = $el.attr("data-pdataMap");
+		var fullName = pdataMap.split(" ")[0];
+		var origName = fullName.split(".")[3];
+		var origValue = $el.attr("data-pdataval");
+		var name = SCORE_CONFIG[origName].name;
+		var filter = SCORE_CONFIG[origName].filter;
+		var value = filter != undefined ? filter(origValue) : origValue;
+		scores[name] = value;
+	});
+	return scores;
 }
 
 function insertStr(str, index, insert) {
@@ -18,41 +147,21 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	$musics.each(function() {
 		var $music = $(this);
 		var seq = parseInt($music.find('.cl_music_data_seq').text());
-		
 		var title = $music.find('.cl_music_data_title').text();
-		
+
 		if (!title) return;
+
+		$.each(DIFFICULTY_LIST, function() {
+			var difficulty = this;
+			var scores = getScores(seq, difficulty);
+			var result = $.extend(scores, {
+				seq: seq,
+				title: title,
+				difficulty: difficulty,
+			});
 		
-		var level = parseInt(getScore(seq, "level"));
-		var score = parseInt(getScore(seq, "best_score"));
-		var rank = getScore(seq, "rank");
-		var sjust = parseInt(getScore(seq, "best_judge_superjust"));
-		var just = parseInt(getScore(seq, "best_judge_just"));
-		var good = parseInt(getScore(seq, "best_judge_good"));
-		var miss = parseInt(getScore(seq, "best_judge_miss"));
-		var near = parseInt(getScore(seq, "best_judge_near"));
-		var combo = parseInt(getScore(seq, "max_combo"));
-		var grade = getScore(seq, "grade");
-		grade = insertStr(grade, grade.length - 2, '.'); // Floatにすると誤差がでるので文字列のまま
-		var datetime =  getScore(seq, "best_time") + ":00";
-	
-		var result = {
-			seq: seq,
-			title: title,
-			level: level,
-			score: score,
-			rank: rank,
-			sjust: sjust,
-			just: just,
-			good: good,
-			miss: miss,
-			near: near,
-			combo: combo,
-			grade: grade,
-			datetime: datetime,
-		};
-		
-		resultList.push(result);
+			resultList.push(result);
+		});
 	});
 
 	sendResponse(resultList);
